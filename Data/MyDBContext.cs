674cf78b7172c4c881d5d7a289c02ccde5b045b5
Franco <franco.torrez.alv@gmail.com>
2024-10-15 08:41:44 -0400

    first commit

---
+++ Data/MyDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace cpDan.Data;
+
+public class MyDBContext : IdentityDbContext<IdentityUser>
+{ 
+    public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
+}
